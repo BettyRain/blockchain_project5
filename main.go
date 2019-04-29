92df196e4022633c213db6731ec0ae6b02b4117e
Betty <liza.ozerova@list.ru>
2019-04-28 22:07:19 -0700

    Made user application running on localhosts from 8813

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"./p3"
-	"./p5"
 	"log"
 	"net/http"
 	"os"
@@ -15,13 +14,4 @@ func main() {
 	} else {
 		log.Fatal(http.ListenAndServe(":6686", router))
 	}
-
-	//TODO: how we can make two parallel routers running? Do a new main?
-	routerUser := p5.NewRouter()
-	if len(os.Args) > 1 {
-		log.Fatal(http.ListenAndServe(":"+os.Args[1], routerUser))
-	} else {
-		log.Fatal(http.ListenAndServe(":6613", routerUser))
-	}
-
 }
---
+++ userApp.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"./p5"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	routerUser := p5.NewRouter()
+	if len(os.Args) > 1 {
+		log.Fatal(http.ListenAndServe(":"+os.Args[1], routerUser))
+	} else {
+		log.Fatal(http.ListenAndServe(":8813", routerUser))
+	}
+
+}
